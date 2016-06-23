96a831742371422db1ccf9b2717e08118c53ae27
Misumi Rize <r@ayase-e.li>
2016-06-23 18:03:21 +0900

    Initial Commit

---
+++ PickToS3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PickToS3
+//
+//  Created by hoaxster on 2016/06/23.
+//  Copyright © 2016年 Rize MISUMI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
